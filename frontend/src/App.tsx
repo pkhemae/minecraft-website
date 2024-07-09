910620936a501972f9b1b15ca7c89ef04435c680
pkhemae <parckhemara@gmail.com>
2024-07-09 10:52:58 +0200

    frontend base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        tailwind test
+      </h1>
+    </div>
+  );
+}
+
+export default App;
