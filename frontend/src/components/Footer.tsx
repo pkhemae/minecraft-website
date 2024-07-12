c63da403fa45ad6ce48158b6743a6975d30318d9
pkhemae <parckhemara@gmail.com>
2024-07-12 13:41:41 +0200

    editing footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -14,14 +14,14 @@ const Footer: React.FC = () => {
             <div className="flex-1">
               <div className="flex flex-col md:flex-row">
                 <div className="w-full md:w-1/2 lg:w-1/3 text-left md:pr-4 mb-6 md:mb-0">
-                  <h3 className="text-yellow-400 text-2xl md:text-4xl font-bold mb-4">A PROPOS</h3>
+                  <h3 className="text-gradient text-2xl md:text-4xl font-bold mb-4">A PROPOS</h3>
                   <p>
                     <span className="text-purple-400 font-bold">{websiteName}</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis amet sunt mollitia atque, nemo autem? Beatae totam maxime possimus quo minus consectetur at eos cum, minima excepturi laudantium harum velit!
                   </p>
                 </div>
 
                 <div className="w-full md:w-1/2 lg:w-1/3 text-left md:pl-4">
-                  <h3 className="text-yellow-400 text-2xl md:text-4xl font-bold mb-4">LIENS UTILES</h3>
+                  <h3 className="text-gradient text-2xl md:text-4xl font-bold mb-4">LIENS UTILES</h3>
                   <ul className="text-xl md:text-2xl">
                     <li><a href="#" className="hover:text-purple-400 text-purple-700 font-extrabold">Nous rejoindre</a></li>
                     <li><a href="#" className="hover:text-purple-400 text-purple-700 font-extrabold ">Boutique</a></li>
@@ -39,7 +39,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="mt-8 flex justify-center items-center bg-gray-800 py-8">
-          <p className="text-center text-sm">
+          <p className="text-center text-base">
             Réalisé avec ❤️ - © 2019-2022 {websiteName}. Tous droits réservés.
           </p>
         </div>
