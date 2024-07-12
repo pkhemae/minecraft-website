1e3eddd61dd32e4411a98fd9364c7f6ac2d2e8e6
pkhemae <parckhemara@gmail.com>
2024-07-12 13:29:40 +0200

    adding footer component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer'
 
 const Home: React.FC = () => {
     return (
         <div>
-            Accueil
+            Home
+            <Footer />
         </div>
     );
 };
