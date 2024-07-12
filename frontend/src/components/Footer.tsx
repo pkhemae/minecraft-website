5520493ebe9b5a039a14320a1a37c80fad1da04f
pkhemae <parckhemara@gmail.com>
2024-07-12 19:24:56 +0200

    editing footer and adding home section

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,50 +1,29 @@
 import React from 'react';
-import siteIcon from '../assets/react.svg';
-
-const websiteName = import.meta.env.VITE_WEBSITE_NAME || 'Server';
 
 const Footer: React.FC = () => {
+
+  const websiteName = import.meta.env.VITE_WEBSITE_NAME;  
+
   return (
-    <div>
-      <div className="bg-yellow-400 h-1"></div>
-
-      <footer className="bg-gray-900 text-white pt-2">
-        <div className="container mx-auto px-4 py-10">
-          <div className="flex flex-col md:flex-row justify-between items-start">
-            <div className="flex-1">
-              <div className="flex flex-col md:flex-row">
-                <div className="w-full md:w-1/2 lg:w-1/3 text-left md:pr-4 mb-6 md:mb-0">
-                  <h3 className="text-gradient text-2xl md:text-4xl font-bold mb-4">A PROPOS</h3>
-                  <p>
-                    <span className="text-purple-400 font-bold">{websiteName}</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis amet sunt mollitia atque, nemo autem? Beatae totam maxime possimus quo minus consectetur at eos cum, minima excepturi laudantium harum velit!
-                  </p>
-                </div>
-
-                <div className="w-full md:w-1/2 lg:w-1/3 text-left md:pl-4">
-                  <h3 className="text-gradient text-2xl md:text-4xl font-bold mb-4">LIENS UTILES</h3>
-                  <ul className="text-xl md:text-2xl">
-                    <li><a href="#" className="hover:text-purple-400 text-purple-700 font-extrabold">Nous rejoindre</a></li>
-                    <li><a href="#" className="hover:text-purple-400 text-purple-700 font-extrabold ">Boutique</a></li>
-                    <li><a href="#" className="hover:text-purple-400 text-purple-700 font-extrabold ">CGV / CGU</a></li>
-                    <li><a href="#" className="hover:text-purple-400 text-purple-700 font-extrabold ">Mentions légales</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex-shrink-0 mx-auto md:mx-10 mt-6 md:mt-0">
-              <img src={siteIcon} alt="Site Icon" className="footer-icon"/>
-            </div>
-          </div>
+    <footer className="bg-black text-white text-center py-4">
+        <div className="text-lg">
+            <span>
+            Copyright © 2023 {websiteName} - Tous droits réservés. |{' '}
+            <a href="#" className="hover:underline text-yellow-300">
+                CGU
+            </a>{' '}
+            |{' '}
+            <a href="#" className="hover:underline text-yellow-300">
+                CGV
+            </a>{' '}
+            | Propulsé par{' '}
+            <a href="https://azuriom.com" className="hover:underline text-yellow-300">
+                {websiteName}
+            </a>
+            .
+            </span>
         </div>
-
-        <div className="mt-8 flex justify-center items-center bg-gray-800 py-8">
-          <p className="text-center text-base">
-            Réalisé avec ❤️ - © 2019-2022 {websiteName}. Tous droits réservés.
-          </p>
-        </div>
-      </footer>
-    </div>
+    </footer>
   );
 };
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
 
 const Home: React.FC = () => {
-    return (
-        <div>
-            Home
-            <Footer />
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold my-8">Home</h1>
+      <div className="flex flex-col md:flex-row justify-around py-8 space-y-4 md:space-y-0 md:space-x-4 px-4">
+        <div className="flex-1 bg-gray-200 rounded-lg shadow-lg p-10 text-center border-solid border-2 border-gray-300">
+          <span className="text-5xl font-bold text-yellow-500">995</span>
+          <p className="mt-4 text-lg font-medium text-gray-700">JOUEURS EN LIGNE</p>
         </div>
-    );
+        <div className="flex-1 bg-gray-200 rounded-lg shadow-lg p-10 text-center border-solid border-2 border-gray-300">
+          <span className="text-5xl font-bold text-yellow-500">+10000</span>
+          <p className="mt-4 text-lg font-medium text-gray-700">JOUEURS UNIQUES</p>
+        </div>
+        <div className="flex-1 bg-gray-200 rounded-lg shadow-lg p-10 text-center border-solid border-2 border-gray-300">
+          <span className="text-5xl font-bold text-yellow-500">2530</span>
+          <p className="mt-4 text-lg font-medium text-gray-700">MEMBRES DISCORD</p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
