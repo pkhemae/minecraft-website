8e657be2ba7bb4e8cb2ab2f7451d00a9929b29e5
pkhemae <parckhemara@gmail.com>
2025-04-29 21:52:16 +0200

    feat: implement dependabot

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
 const Home: React.FC = () => {
     const websiteName = import.meta.env.VITE_WEBSITE_NAME;
 
     return (
         <div>
-            <Helmet>
-                <title>{websiteName} | Accueil</title>
-            </Helmet>
             <p className="text-red-500">bonjour</p>
         </div>
     );
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
 const NotFound: React.FC = () => {
     const websiteName = import.meta.env.VITE_WEBSITE_NAME;
 
     return (
         <div>
-            <Helmet>
-                <title>{websiteName} | 404</title>
-            </Helmet>
             <div>
                 <p>404</p>
             </div>
